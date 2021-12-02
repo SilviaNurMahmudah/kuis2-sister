5a8bb6de24dd5b94d4c8af727c038cf959cf94a5
SilviaNurMahmudah <silvia.mahmudah@gmail.com>
2021-12-02 13:18:54 +0700

    kuis2-sister

---
+++ php/src/koneksi.php
@@ -0,0 +1,18 @@
+<?php 
+
+$servername = "172.20.0.2";
+$username = "root";
+$password = "";
+$dbname="db_arsip2";
+
+$koneksi = mysqli_connect('172.20.0.2', 'root', '');
+$db = mysqli_select_db($koneksi ,$dbname);
+
+global $conn;
+$conn = mysqli_connect($servername, $username, $password,$dbname);
+// Check connection
+if (!$conn) {
+	die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
