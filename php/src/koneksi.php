ba88ec20cb5ee6a5d1da9ba6bc4620c98ab3cf49
SilviaNurMahmudah <72615487+SilviaNurMahmudah@users.noreply.github.com>
2021-12-27 21:17:50 +0700

    Update koneksi.php

--- php/src/koneksi.php
+++ php/src/koneksi.php
@@ -5,9 +5,6 @@ $username = "root";
 $password = "admin";
 $dbname="db_arsip2";
 
-$koneksi = mysqli_connect('172.20.0.2', 'root', 'admin');
-$db = mysqli_select_db($koneksi ,$dbname);
-
 global $conn;
 $conn = mysqli_connect($servername, $username, $password,$dbname);
 // Check connection
